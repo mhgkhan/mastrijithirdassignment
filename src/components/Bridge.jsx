33f41dd14726378c37642888f16431f8915b6145
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-08-16 17:17:47 +0500

    Added Footer

--- src/components/Bridge.jsx
+++ src/components/Bridge.jsx
@@ -3,6 +3,7 @@ import RandomUser from "../pages/RandomUser";
 import Header from "./Header";
 import RandomJookies from "../pages/RandomJookies";
 import Catlisting from "../pages/Catlisting";
+import Footer from "./Footer";
 
 export default function Bridge() {
     return <>
@@ -15,6 +16,7 @@ export default function Bridge() {
                 <Route path="/random-jokes" element={<RandomJookies />} />
                 <Route path="/cats-listing" element={<Catlisting />} />
             </Routes>
+            <Footer />
 
 
         </BrowserRouter>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className=' bg-black'>
+        <div className="container mx-auto">
+            <p className='text-center p-3 text-lg text-white mt-2'>Copyright&copy; Allright Reserved <a href="https://ghaznaprogrammar.com">Ghazna Programmar GH</a></p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
