51eda8341e59b3bf9ba080289bd4e568fe72176f
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-08-16 10:42:41 +0500

    Created Bridge File For Handling Routes

--- src/components/Bridge.jsx
+++ src/components/Bridge.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 export default function Bridge(){
     return <>
     
-    
+        <BrowserRouter>
+        
+        <Routes>
+            <Route path="/" element={""} />
+        </Routes>
+        
+        
+        </BrowserRouter>
     
     </>
 }
